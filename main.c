009ea93b393df9c01725c695a320a55a09d15ff0
Wesley <wesley101201@gmail.com>
2018-08-18 01:16:54 -0300

    aa

--- main.c
+++ main.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+struct tel{
+  int ddd, cel, trab, casa;
+};
+struct regagenda{
+  char nome[100], endereco[300];
+  struct tel fone;
+};
+struct regagenda agenda;
 
 int main (void){
   int ;
   float ;
   FILE *arquivoUM;
-  arquivoUM = fopen("arquivoBin.dat", "rb");
+  arquivoUM = fopen("arquivoBin.dat", "wb");
+
+
   if (arquivoUM == NULL){
 
     printf("Problema na abertura do arquivo.\n");
