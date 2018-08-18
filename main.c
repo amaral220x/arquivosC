861ae9c1122d455a06b3247e22862124aee0f20b
Wesley <wesley101201@gmail.com>
2018-08-18 12:10:12 -0300

    funcionando a leitura dos arquivos

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 struct tel{
   int ddd, cel, trab, casa;
 };
@@ -8,19 +10,48 @@ struct regagenda{
 };
 struct regagenda agenda;
 
+void preencher(){
+  printf("Entre com o nome: ");
+  gets(agenda.nome);
+  printf("\nEntre com o endereço: ");
+  gets(agenda.endereco);
+  printf("\nEntre com o ddd: ");
+  scanf("%d",&agenda.fone.ddd);
+  printf("\nEntre com o telefone da casa: ");
+  scanf("%d",&agenda.fone.casa);
+  printf("\nEntre com o telefone celular: ");
+  scanf("%d",&agenda.fone.cel);
+  printf("\nEntre com o telefone do trabalho: ");
+  scanf("%d",&agenda.fone.trab);
+  printf("\nNome: %s\nEndereço: %s\nDDD: %d\nCasa: %d\nCelular: %d\nTrabalho: %d\n",agenda.nome,agenda.endereco,agenda.fone.ddd,agenda.fone.casa,agenda.fone.cel,agenda.fone.trab);
+}
+
 int main (void){
-  int ;
+  void preencher();
+  int contador =0 ;
   float ;
+  char opcao;
   FILE *arquivoUM;
-  arquivoUM = fopen("arquivoBin.dat", "wb");
-
-
-  if (arquivoUM == NULL){
-
-    printf("Problema na abertura do arquivo.\n");
-    return ;
-
+  arquivoUM = fopen("arquivoBin.txt", "wb");
+  if (arquivoUM == NULL) {
+    printf("Erro ao abrir o arquivo\n" );
+    system("pause");
   }
+
+    //preencher();
+    contador++;
+    strcpy (agenda.nome, "JATO");
+    strcpy (agenda.endereco, "RUA PINTO");
+    agenda.fone.ddd=11;
+    printf("%d\n",agenda.fone.ddd);
+    printf("%s\n", agenda.nome);
+
+    //fwrite( agenda.nome ,sizeof(agenda.nome),1, arquivoUM);
+    fwrite( &agenda, sizeof(struct regagenda), 1, arquivoUM);
+
+
+    fclose (arquivoUM);
+
   return 0;
 
 }
