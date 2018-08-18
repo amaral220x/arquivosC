0c2d11477d20b069f3c51db9f0f83dfca37a5f6b
amaral220x <biel.ilha2021@gmail.com>
2018-08-18 02:11:56 -0300

    Le tudo

---
+++ aaa.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void preencher();
+struct tel{
+  int ddd, cel, trab, casa;
+};
+struct regagenda{
+  char nome[100], endereco[300];
+  struct tel fone;
+};
+struct regagenda agenda;
+
+void preencher(){
+  printf("Entre com o nome: ");
+  gets(agenda.nome);
+  printf("\nEntre com o endereço: ");
+  gets(agenda.endereco);
+  printf("\nEntre com o ddd: ");
+  scanf("%d",&agenda.fone.ddd);
+  printf("\nEntre com o telefone da casa: ");
+  scanf("%d",&agenda.fone.casa);
+  printf("\nEntre com o telefone celular: ");
+  scanf("%d",&agenda.fone.cel);
+  printf("\nEntre com o telefone do trabalho: ");
+  scanf("%d",&agenda.fone.trab);
+  printf("\nNome: %s\nEndereço: %s\nDDD: %d\nCasa: %d\nCelular: %d\nTrabalho: %d\n",agenda.nome,agenda.endereco,agenda.fone.ddd,agenda.fone.casa,agenda.fone.cel,agenda.fone.trab);
+}
+
+int main (void){
+  int ;
+  float ;
+  FILE *arquivoUM;
+  arquivoUM = fopen("arquivoBin.dat", "wb");
+
+
+  if (arquivoUM == NULL){
+
+    printf("Problema na abertura do arquivo.\n");
+    return ;
+
+  }
+  preencher();
+  return 0;
+  system("pause");
+
+}
