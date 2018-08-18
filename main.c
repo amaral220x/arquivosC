423ebb31dd54b35159453ff32058e24dbfa0b422
Wesley <wesley101201@gmail.com>
2018-08-18 00:59:16 -0300

    a

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void){
+  int ;
+  float ;
+  FILE *arquivoUM;
+  arquivoUM = fopen("arquivoBin.dat", "rb");
+  if (arquivoUM == NULL){
+
+    printf("Problema na abertura do arquivo.\n");
+    return ;
+
+  }
+  return 0;
+  
+}
